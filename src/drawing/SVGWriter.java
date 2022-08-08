bb0db026b0ec8f58c2f0989a2d005fb79e15f357
anutfullina <xxxnut@yandex.ru>
2022-08-08 18:43:42 +0500

    Начало задания 2

---
+++ src/drawing/SVGWriter.java
@@ -0,0 +1,23 @@
+package drawing;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class SVGWriter {
+
+    FileWriter fileWriter;
+    String fileName;
+
+    public SVGWriter(String fileName){
+        this.fileName = fileName;
+
+    }
+
+    public void writeHeader(){
+
+    }
+
+    public void writeHFooter(){
+
+    }
+}
